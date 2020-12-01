9f4ca9460d84b66c948184080581996fc974be1e
Ivan <trajchevivan@gmail.com>
2020-12-01 16:36:52 +0100

    Connected to db

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { errorHandler } from './middlewares/error-handler';
 import { currentUserRouter } from './routes/currentuser';
@@ -8,6 +10,8 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { NotFoundError } from './errors/not-found-error';
+
+dotenv.config({path: './config.env'});
 
 const app = express();
 
@@ -24,6 +28,21 @@ app.all('*', async (req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(3200, () => {
-  console.log('Listening on port 3200');
-});
+const start = async () => {
+  try {
+    await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.jxb6m.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Connected to mongoDb')
+  } catch(err) {
+    console.log(err);
+  }
+
+  app.listen(3200, () => {
+    console.log('Listening on port 3200');
+  });
+  
+}
+
+start();
