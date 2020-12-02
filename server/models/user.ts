092632fed0c9697e2b8f8a13a5dc7efce0458601
Ivan <trajchevivan@gmail.com>
2020-12-02 19:07:44 +0100

    Added pre save hook

--- server/models/user.ts
+++ server/models/user.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+import { Password } from '../services/password';
+
 interface IUserAttrs {
   email: string;
   password: string;
@@ -23,6 +25,16 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+});
+
+userSchema.pre('save', async function(done) {
+
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed)
+  }
+
+  done(null);
 });
 
 userSchema.method('build', (attrs: IUserAttrs) => {
--- server/routes/signup.ts
+++ server/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response} from 'express';
 import { body, validationResult } from 'express-validator';
+
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -13,7 +15,7 @@ router.post('/api/users/signup', [
     .trim()
     .isLength({min: 4, max: 20})
     .withMessage('Password must be between 4 and 20 characters long')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
 
   const errors = validationResult(req);
 
@@ -22,12 +24,20 @@ router.post('/api/users/signup', [
   }
 
   const { email, password } = req.body;
-  throw new DatabaseConnectionError();
 
-  // console.log('Creating a user ...')
-
+  console.log(email, password)
   
-  // res.send({})
-})
+  const existingUser = await User.findOne({email});
+
+  if (existingUser) {
+    throw new BadRequestError('Email in use')
+  }
+
+  const user = User.build({email, password});
+  await user.save();
+
+  res.status(201).send(user);
+
+});
 
 export { router as signupRouter }
